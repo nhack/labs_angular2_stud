c1d079ab908e18ad10fea895c3f4874201190967
Marius Cristea <marius.cristea@irian.ro>
2016-04-05 14:36:02 +0300

    L03 - NG - if

--- L03_AH/app/app.component.ts
+++ L03_AH/app/app.component.ts
@@ -1,19 +1,22 @@
 import {Component} from 'angular2/core';
+import {Pizza} from './pizza';
 
 @Component({
-    selector: 'my-app',
-    template: ''
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
 
-    pizza;
+  private pizza: Pizza;
 
-    constructor() {
-        this.pizza = {
-            name: '4 STAGIONI',
-            price: 27.5,
-            weight: 550,
-            ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare'
-        }
+  constructor() {
+    this.pizza = {
+      name: '4 STAGIONI',
+      price: 27.5,
+      weight: 550,
+      ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare',
+      canPurchase: true,
+      soldOut: false
     }
+  }
 }
---
+++ L03_AH/app/pizza.ts
@@ -0,0 +1,8 @@
+export interface Pizza{
+  name: string,
+  price: number,
+  weight: number,
+  ingredients: string,
+  canPurchase: boolean,
+  soldOut: boolean
+}
