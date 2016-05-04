dfa266992a182184e46acfe50d607d15eda8eb35
Marius Cristea <marius.cristea@irian.ro>
2016-05-04 15:30:11 +0300

    L06 - NG - Service

---
+++ L06_AS/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component} from 'angular2/core';
+import {ViewEncapsulation} from 'angular2/core';
+import {TabsComponent} from './tabs.component';
+import {Pizza} from './pizza';
+import {PIZZAS} from './data';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  directives: [TabsComponent]
+})
+export class AppComponent {
+
+  private pizzas: Pizza[];
+
+  constructor() {
+    this.pizzas = PIZZAS;
+  }
+}
